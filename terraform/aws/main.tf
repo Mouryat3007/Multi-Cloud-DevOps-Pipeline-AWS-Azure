552903ff5d85ddcfa570f7ef322cd09d830dcbfa
Mourya Teja Padmanabhuni <92813285+Mouryat3007@users.noreply.github.com>
2025-05-15 18:06:29 +0100

    Update main.tf

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,19 +1,37 @@
-variable "aws_region" {
-  type = string
-  default = "us-east-1"
+provider "aws" {
+  region = var.aws_region
 }
 
-variable "cluster_name" {
-  type = string
-  default = "my-eks-cluster"
+data "aws_vpc" "default" {
+  count   = var.vpc_id == "" ? 1 : 0
+  default = true
 }
 
-variable "vpc_id" {
-  type = string
-  default = "vpc-0f86ea073cfae2743 "
+data "aws_subnets" "default" {
+  count = length(var.subnets) == 0 ? 1 : 0
+
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default[0].id]
+  }
 }
 
-variable "subnet_ids" {
-  type = list(string)
-  default = ["subnet-0cc2f2bdb2f18e273"]
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+
+  vpc_id  = var.vpc_id != "" ? var.vpc_id : data.aws_vpc.default[0].id
+  subnets = length(var.subnets) > 0 ? var.subnets : data.aws_subnets.default[0].ids
+
+  manage_aws_auth = true
+
+  node_groups = {
+    default = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+      instance_types   = ["t3.medium"]
+    }
+  }
 }
