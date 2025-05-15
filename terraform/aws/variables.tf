30709b9470d1af78c33285eef207a641be7083a2
Mourya Teja Padmanabhuni <92813285+Mouryat3007@users.noreply.github.com>
2025-05-15 17:42:57 +0100

    Update variables.tf

--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -1,6 +1,21 @@
-variable "aws_region" {}
-variable "cluster_name" {}
-variable "subnets" {
-  type = list(string)
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
 }
-variable "vpc_id" {}
+
+variable "cluster_name" {
+  type    = string
+  default = "my-eks-cluster"
+}
+
+variable "vpc_id" {
+  type    = string
+  default = ""
+  description = "Use default VPC if empty"
+}
+
+variable "subnets" {
+  type    = list(string)
+  default = []
+  description = "Use default VPC subnets if empty"
+}
