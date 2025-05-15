3738e4103e6fc6ca08ac67acb5f6aa4082e08e12
Mourya Teja Padmanabhuni <92813285+Mouryat3007@users.noreply.github.com>
2025-05-15 18:06:43 +0100

    Update variables.tf

--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -10,12 +10,12 @@ variable "cluster_name" {
 
 variable "vpc_id" {
   type    = string
-  default = ""
+  default = "vpc-0f86ea073cfae2743"
   description = "Use default VPC if empty"
 }
 
 variable "subnets" {
   type    = list(string)
-  default = []
+  default = ["subnet-0cc2f2bdb2f18e273"]
   description = "Use default VPC subnets if empty"
 }
