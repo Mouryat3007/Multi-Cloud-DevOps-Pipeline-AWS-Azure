92a0a58167f5d64754b782d1cb2e5d5dd09d6611
Mourya Teja Padmanabhuni <92813285+Mouryat3007@users.noreply.github.com>
2025-05-15 17:43:11 +0100

    Update main.tf

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -2,13 +2,30 @@ provider "aws" {
   region = var.aws_region
 }
 
+data "aws_vpc" "default" {
+  count   = var.vpc_id == "" ? 1 : 0
+  default = true
+}
+
+data "aws_subnets" "default" {
+  count = length(var.subnets) == 0 ? 1 : 0
+
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default[0].id]
+  }
+}
+
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = var.cluster_name
   cluster_version = "1.29"
-  subnets         = var.subnets
-  vpc_id          = var.vpc_id
+
+  vpc_id  = var.vpc_id != "" ? var.vpc_id : data.aws_vpc.default[0].id
+  subnets = length(var.subnets) > 0 ? var.subnets : data.aws_subnets.default[0].ids
+
   manage_aws_auth = true
+
   node_groups = {
     default = {
       desired_capacity = 2
