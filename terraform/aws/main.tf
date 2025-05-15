9b7a4284fb02a1e31e8f754e7c28886aa7ab1d14
Mourya Teja Padmanabhuni <92813285+Mouryat3007@users.noreply.github.com>
2025-05-15 17:50:37 +0100

    Update main.tf

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -18,7 +18,7 @@ data "aws_subnets" "default" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "18.29.0"  # specify exact working version
+  version = "17.24.0"  # specify exact working version
 
   cluster_name    = var.cluster_name
   cluster_version = "1.29"
